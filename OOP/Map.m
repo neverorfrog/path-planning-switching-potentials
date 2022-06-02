3b5aa223af6eb9cc361a3a2288626348d2e7da93
neverorfrog <97flavio.maiorana@gmail.com>
2022-06-03 00:32:30 +0200

    Pulizia di file vecchi

--- OOP/Map.m
+++
@@ -1,8 +0,0 @@
-classdef Map < occupancyMap
-    
-    methods
-        function obj = Map(width,height,resolution)
-            obj@occupancyMap(width,height,resolution);
-        end
-    end
-end
